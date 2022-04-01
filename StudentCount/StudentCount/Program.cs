0408255ef018d1a538b996f7e550942903dbc3f4
ibrahimaliyevv2 <7cauc3b@code.edu.az>
2022-04-01 18:14:00 +0400

    formtasks added

---
+++ StudentCount/StudentCount/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StudentCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            Console.WriteLine(student1.No);
+
+            Student student2 = new Student();
+            Console.WriteLine(student2.No);
+        }
+    }
+}
---
+++ StudentCount/StudentCount/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentCount
+{
+    class Student
+    {
+        public Student()
+        {
+            TotalCount++;
+            No = TotalCount;
+        }
+
+  
+        public int No;
+        public static int TotalCount;
+        public string FullName;
+        public string GroupNo;
+      
+    }
+}
